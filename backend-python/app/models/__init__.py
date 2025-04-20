42fd0f1913000f532c74bd5de11e46286e3ec0da
faust-lvii <k.yusufokuducu@gmail.com>
2025-04-20 20:48:28 +0300

    cicd0.12

--- backend-python/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-from app.models.friendship import Friendship, FriendshipStatus
-from app.models.message import Message
-
-# For Alembic migrations
-from app.db.base import Base
--- backend-python/run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-# Run pytest with coverage report
-pytest --cov=app --cov-report=term-missing tests/
